93a9cecfe7489249853a1ff0918b804fe41db48c
Carlos Sanchez <csanchez@maestrodev.com>
2013-09-04 19:11:42 +0200

    Use ensure_packages from stdlib to install the dependencies

--- Modulefile
+++ Modulefile
@@ -7,3 +7,5 @@ project_page 'http://github.com/maestrodev/puppet-capybara'
 source 'http://github.com/maestrodev/puppet-capybara'
 summary 'Capybara testing framework installation'
 description 'Install capybara and webkit with their dependencies'
+
+dependency 'puppetlabs/stdlib', '>=3.2.0'
--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,8 +1,3 @@
 class capybara::dependencies {
-
-  if ! defined(Package['libxml2'])         { package { 'libxml2':         ensure => installed } }
-  if ! defined(Package['libxml2-devel'])   { package { 'libxml2-devel':   ensure => installed } }
-  if ! defined(Package['libxslt'])         { package { 'libxslt':         ensure => installed } }
-  if ! defined(Package['libxslt-devel'])   { package { 'libxslt-devel':   ensure => installed } }
-
+  ensure_packages(['libxml2','libxml2-devel','libxslt','libxslt-devel'])
 }
